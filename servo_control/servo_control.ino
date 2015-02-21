fd0136609b477e9a21c0184c3a4b2159ad17e8b9
Mathijs de Langen <langen@robot-rose.nl>
2015-02-21 16:50:03 +0100

    added some constants

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -2,28 +2,34 @@
 
 Servo myservo;
 
+// set pin numbers:
+const int servoPin     = 9;     // the number of the servo pin
+
+// set servo characteristics
+const int servo_offset = 11;    // The offset of the servo
+
 void setup()
 {
-  myservo.attach(9);
+  myservo.attach(servoPin);
 }
 
 void loop()
 {
-  myservo.writeMicroseconds(1511);
+  myservo.writeMicroseconds(1500+servo_offset);
   delay(10000);
   
-  myservo.writeMicroseconds(1450);
+  myservo.writeMicroseconds(1450+servo_offset);
   delay(1000);
   
-  myservo.writeMicroseconds(1400);
+  myservo.writeMicroseconds(1400+servo_offset);
   delay(1000);
   
-  myservo.writeMicroseconds(1350);
+  myservo.writeMicroseconds(1350+servo_offset);
   delay(1000);
   
-  myservo.writeMicroseconds(1300);
+  myservo.writeMicroseconds(1300+servo_offset);
   delay(1000);
   
-  myservo.writeMicroseconds(2000);
+  myservo.writeMicroseconds(2000+servo_offset);
   delay(1000);
 }
