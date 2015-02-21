f9cb98b4d611b7b707696bc15bb1dd611957d4b6
Mathijs de Langen <langen@robot-rose.nl>
2015-02-21 18:40:07 +0100

    renamed myservo and servoPin

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -1,38 +1,38 @@
 #include <Servo.h>
 
-Servo myservo;
+Servo rotating_servo;
 
 // set pin numbers:
-const int servoPin     = 9;     // the number of the servo pin
+const int servo_pin     = 9;     // the number of the servo pin
 
 // set servo characteristics
 const int servo_offset = 11;    // The offset of the servo
 
 void setup()
 {
-  myservo.attach(servoPin);
+  rotating_servo.attach(servo_pin);
 }
 
 void loop()
 {
   // Stop the servo
-  myservo.writeMicroseconds(1500+servo_offset);
+  rotating_servo.writeMicroseconds(1500+servo_offset);
   delay(10000); // Wait for 10s
   
   // Rotate with different speeds
-  myservo.writeMicroseconds(1450+servo_offset);
+  rotating_servo.writeMicroseconds(1450+servo_offset);
   delay(1000);
   
-  myservo.writeMicroseconds(1400+servo_offset);
+  rotating_servo.writeMicroseconds(1400+servo_offset);
   delay(1000);
   
-  myservo.writeMicroseconds(1350+servo_offset);
+  rotating_servo.writeMicroseconds(1350+servo_offset);
   delay(1000);
   
-  myservo.writeMicroseconds(1300+servo_offset);
+  rotating_servo.writeMicroseconds(1300+servo_offset);
   delay(1000);
   
   // Rotate the other way
-  myservo.writeMicroseconds(2000+servo_offset);
+  rotating_servo.writeMicroseconds(2000+servo_offset);
   delay(1000);
 }
