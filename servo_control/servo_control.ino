c63c3a390a6f2bfc42094d74a4abbaac01874a76
Mathijs de Langen <langen@robot-rose.nl>
2015-04-09 22:29:35 +0200

    Added left/right movement functionality

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -3,52 +3,77 @@
 Servo rotating_servo;
 
 // Set pin numbers:
-const int switch_pin    = 6;    // The ID of the switch pin
-const int servo_pin     = 9;    // The ID of the servo pin
+const int manual_control_pin = 6;  // The ID of the switch pin
+const int servo_pin          = 9;  // The ID of the servo pin
 
 // Set servo characteristics
-const int servo_offset = 11;    // The offset of the servo
+const int servo_offset       = 11; // The offset of the servo
 
-int button_state = 0;           // The state of the button
+enum movement {
+  MOVING_LEFT,
+  MOVING_RIGHT,
+  IDLE
+};
+
+int button_state             = 0;           // The state of the button
+movement current_movement    = IDLE;        // Current movement
+movement prev_movement       = MOVING_LEFT; // Previous movement (right or left)
 
 void setup()
 {
   rotating_servo.attach(servo_pin);
   
-  pinMode(switch_pin, INPUT);
+  pinMode(manual_control_pin, INPUT);
 }
 
 void loop()
 {
   // Read button value
-  button_state = digitalRead(switch_pin);
+  button_state = digitalRead(manual_control_pin);
   
-  // Stop the servo
-  //rotating_servo.writeMicroseconds(1500+servo_offset);
-  //delay(10000); // Wait for 10s
-    
-  if (button_state == HIGH)
-  {    
-    // Rotate with different speeds
-    rotating_servo.writeMicroseconds(1450+servo_offset);
-    delay(100);
-    
-    rotating_servo.writeMicroseconds(1400+servo_offset);
-    delay(100);
-    
-    rotating_servo.writeMicroseconds(1350+servo_offset);
-    delay(100);
-    
-    rotating_servo.writeMicroseconds(1300+servo_offset);
-    delay(100);
+  // Button pressed
+  if ( button_state == HIGH )
+  {
+    if ( current_movement == IDLE )
+    {
+      if ( prev_movement == MOVING_LEFT )
+      {
+        MoveRight();
+      }
+      else
+      {
+        MoveLeft();
+      }
+    }
   }
-  
-  if (button_state == LOW)
-  { 
-    // Rotate the other way
-    rotating_servo.writeMicroseconds(2000+servo_offset);
-    delay(100);
+
+  // Button not pressed
+  if ( button_state == LOW )
+  {
+    if ( current_movement == MOVING_LEFT || current_movement == MOVING_RIGHT )
+    {
+      StopMoving();
+    }
   }
-  
-  //delay(1000);
+
+  delay(1000); // Wait a second
+}
+
+void MoveLeft()
+{
+  current_movement = MOVING_LEFT;
+  rotating_servo.writeMicroseconds(1300+servo_offset);
+}
+
+void MoveRight()
+{
+  current_movement = MOVING_RIGHT;
+  rotating_servo.writeMicroseconds(2000+servo_offset);
+}
+
+void StopMoving()
+{
+  prev_movement    = current_movement;
+  current_movement = IDLE;
+  rotating_servo.writeMicroseconds(1500+servo_offset);
 }
