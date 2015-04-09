5d86c90db4c76f11013a19feea5464db203f91ca
Mathijs de Langen <langen@robot-rose.nl>
2015-04-09 22:16:13 +0200

    added switch

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -2,37 +2,53 @@
 
 Servo rotating_servo;
 
-// set pin numbers:
-const int servo_pin     = 9;     // the number of the servo pin
+// Set pin numbers:
+const int switch_pin    = 6;    // The ID of the switch pin
+const int servo_pin     = 9;    // The ID of the servo pin
 
-// set servo characteristics
+// Set servo characteristics
 const int servo_offset = 11;    // The offset of the servo
+
+int button_state = 0;           // The state of the button
 
 void setup()
 {
   rotating_servo.attach(servo_pin);
+  
+  pinMode(switch_pin, INPUT);
 }
 
 void loop()
 {
+  // Read button value
+  button_state = digitalRead(switch_pin);
+  
   // Stop the servo
-  rotating_servo.writeMicroseconds(1500+servo_offset);
-  delay(10000); // Wait for 10s
+  //rotating_servo.writeMicroseconds(1500+servo_offset);
+  //delay(10000); // Wait for 10s
+    
+  if (button_state == HIGH)
+  {    
+    // Rotate with different speeds
+    rotating_servo.writeMicroseconds(1450+servo_offset);
+    delay(100);
+    
+    rotating_servo.writeMicroseconds(1400+servo_offset);
+    delay(100);
+    
+    rotating_servo.writeMicroseconds(1350+servo_offset);
+    delay(100);
+    
+    rotating_servo.writeMicroseconds(1300+servo_offset);
+    delay(100);
+  }
   
-  // Rotate with different speeds
-  rotating_servo.writeMicroseconds(1450+servo_offset);
-  delay(1000);
+  if (button_state == LOW)
+  { 
+    // Rotate the other way
+    rotating_servo.writeMicroseconds(2000+servo_offset);
+    delay(100);
+  }
   
-  rotating_servo.writeMicroseconds(1400+servo_offset);
-  delay(1000);
-  
-  rotating_servo.writeMicroseconds(1350+servo_offset);
-  delay(1000);
-  
-  rotating_servo.writeMicroseconds(1300+servo_offset);
-  delay(1000);
-  
-  // Rotate the other way
-  rotating_servo.writeMicroseconds(2000+servo_offset);
-  delay(1000);
+  //delay(1000);
 }
